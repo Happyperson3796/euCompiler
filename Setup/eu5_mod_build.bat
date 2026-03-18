0925f991c03b548ebcb56ee7ec93b4863e725809
BuildTools <unconfigured@null.spigotmc.org>
2026-03-18 14:22:18 -0400

    Include setup files

---
+++ Setup/eu5_mod_build.bat
@@ -0,0 +1,3 @@
+@echo off
+python %~dp0\eu5_mod_build.py "%CD%"
+pause
---
+++ Setup/eu5_mod_build.py
@@ -0,0 +1,5 @@
+import eu5.compiler
+import sys
+
+print("Running build file in "+sys.argv[-1])
+eu5.compiler.Build(sys.argv[-1]).build()
